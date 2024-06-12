b499838404bc9b2b8bc627a22c179635a5a78157
Frozenheart32 <alexroid32@gmail.com>
2024-06-12 18:27:19 +0300

    ASC and AttributeSet

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -8,9 +8,9 @@ public class Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PublicIncludePaths.AddRange(
 			new string[]
