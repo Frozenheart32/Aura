fc968d715bb6461fbbea96b76c3829debd9e7de0
Frozenheart32 <alexroid32@gmail.com>
2024-06-21 21:47:31 +0300

    Send Mouse Cursor Data

--- Source/Aura/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
@@ -12,8 +14,37 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
-	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if(bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		//TODO: We are on the server, so listen for target data
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopedPrediction{AbilitySystemComponent.Get()};
+
+	const APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	ValidData.Broadcast(CursorHit.Location);
+	
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit{};
+	Data->HitResult = CursorHit;
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	DataHandle.Add(Data);
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(),
+		GetActivationPredictionKey(), DataHandle, FGameplayTag{},
+		AbilitySystemComponent->ScopedPredictionKey);
+
+	if(ShouldBroadcastAbilityTaskDelegates())
+	{
+		//ValidData.Broadcast(DataHandle);
+	}
 }
--- Source/Aura/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -27,4 +27,5 @@ public:
 protected:
 
 	virtual void Activate() override;
+	void SendMouseCursorData();
 };
