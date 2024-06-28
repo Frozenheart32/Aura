d47b4ec8740d70c8373481fa5268312d0a1bc8ce
Frozenheart32 <alexroid32@gmail.com>
2024-06-28 19:56:38 +0300

    Using EQS Queries in Behavior Tree

--- Source/Aura/Player/AuraPlayerController.cpp
+++ Source/Aura/Player/AuraPlayerController.cpp
@@ -166,10 +166,12 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 					Spline->AddSplinePoint(PointLocation, ESplineCoordinateSpace::World);
 					//DrawDebugSphere(GetWorld(), PointLocation, 8.f, 8, FColor::Green, false, 5.f);
 				}
-				
-				CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1]; 
 
-				bAutoRunning = true;
+				if(NavPath->PathPoints.Num() > 0)
+				{
+					CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1]; 
+					bAutoRunning = true;
+				}
 			}
 		}
 
