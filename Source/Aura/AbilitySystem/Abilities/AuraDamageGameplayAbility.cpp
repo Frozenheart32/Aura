e59b74f254bf6c1945055484a7f1986e8272915b
Frozenheart32 <alexroid32@gmail.com>
2024-07-01 14:21:49 +0300

    Playing the Ranged Attack Montages

--- Source/Aura/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -5,6 +5,18 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
+#include "Interaction/CombatInterface.h"
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontageFromArray(const TArray<FTaggedMontage>& TaggedMontages) const
+{
+	if(TaggedMontages.Num() > 0)
+	{
+		const int32 Selection = FMath::RandRange(0, TaggedMontages.Num() - 1);
+		return TaggedMontages[Selection];
+	}
+
+	return FTaggedMontage{};
+}
 
 void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 {
