c5b43e0481b5cdd6a0f062cbeb7465b1b58281eb
Frozenheart32 <alexroid32@gmail.com>
2024-07-11 00:54:22 +0300

    Top off Our Fluids

--- Source/Aura/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/AbilitySystem/AuraAttributeSet.h
@@ -167,9 +167,13 @@ public:
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
 	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 private:
+
+	bool bTopOffHealth = false;
+	bool bTopOffMana = false;
 
 	void SendXPEvent(const FEffectProperties& Props);
 
