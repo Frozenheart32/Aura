21f1526c3b09de394b963989a6ed9f1debebeb48
Frozenheart32 <alexroid32@gmail.com>
2024-07-23 20:45:21 +0300

    Firebolt Projectile Spread

--- Source/Aura/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -3,6 +3,10 @@
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
 
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 FString UAuraFireBolt::GetDescription(int32 CurrentLevel) const
 {
 	const int32 ScaledDamage = Damage.GetValueAtLevel(CurrentLevel);
@@ -44,7 +48,7 @@ FString UAuraFireBolt::GetDescription(int32 CurrentLevel) const
 			CurrentLevel,
 			ManaCost,
 			Cooldown,
-			FMath::Min(NumProjectiles, CurrentLevel),
+			FMath::Min(MaxNumProjectiles, CurrentLevel),
 			ScaledDamage);
 	}
 }
@@ -69,6 +73,70 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 CurrentLevel) const
 			NextLevel,
 			ManaCost,
 			Cooldown,
-			FMath::Min(NumProjectiles, NextLevel),
+			FMath::Min(MaxNumProjectiles, NextLevel),
 			ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
+	bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	if(!GetAvatarActorFromActorInfo()->HasAuthority()) return;
+
+	if(ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo()))
+	{
+		const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
+		FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+		if(bOverridePitch)
+		{
+			Rotation.Pitch = PitchOverride;
+		}
+		
+		const FVector Forward = Rotation.Vector();
+		const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+		const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);
+		
+		const int32 NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+		
+		if(NumProjectiles > 1)
+		{
+			const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1);
+			for (int32 i = 0; i < NumProjectiles; i++)
+			{
+				const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+				UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Direction * 100.f,
+			5.f, FLinearColor::Red, 120.f);
+			}
+		}
+		else
+		{
+			//Single Projectile
+			SpawnProjectile(ProjectileTargetLocation, SocketTag, bOverridePitch, PitchOverride);
+		}
+
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 100.f,
+			5.f, FLinearColor::White, 120.f);
+
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.f,
+			1.f, FLinearColor::Gray, 120.f);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.f,
+			1.f, FLinearColor::Gray, 120.f);
+
+		
+		/*
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		SpawnTransform.SetRotation(Rotation.Quaternion());
+
+		const auto SpawnedProjectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetAvatarActorFromActorInfo(),
+			Cast<APawn>(GetOwningActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		
+		SpawnedProjectile->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
+		SpawnedProjectile->FinishSpawning(SpawnTransform);
+		*/
+	}
 }
--- Source/Aura/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/AbilitySystem/Abilities/AuraFireBolt.h
@@ -14,8 +14,22 @@ class AURA_API UAuraFireBolt : public UAuraProjectileSpell
 {
 	GENERATED_BODY()
 
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Firebolt")
+	float ProjectileSpread = 90.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Firebolt")
+	int32 MaxNumProjectiles = 5;
+
 public:
 
 	virtual FString GetDescription(int32 CurrentLevel) const override;
 	virtual FString GetNextLevelDescription(int32 CurrentLevel) const override;
+
+protected:
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintProtected))
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch = false, float PitchOverride = 0.f, AActor* HomingTarget = nullptr);
+
 };
