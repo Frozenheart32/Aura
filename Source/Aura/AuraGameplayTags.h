037dc2153883dae8fe8f2073f07cc90a356a0a88
Frozenheart32 <alexroid32@gmail.com>
2024-08-21 19:51:55 +0300

    Passive Spell Tags

--- Source/Aura/AuraGameplayTags.h
+++ Source/Aura/AuraGameplayTags.h
@@ -96,6 +96,10 @@ public:
 
 	FGameplayTag Abilities_Lightning_Electrocute;
 
+	FGameplayTag Abilities_Passive_HaloOfProtection;
+	FGameplayTag Abilities_Passive_LifeSiphon;
+	FGameplayTag Abilities_Passive_ManaSiphon;
+
 	FGameplayTag Cooldown_Fire_Firebolt;
 
 	FGameplayTag Cooldown_Lightning_Electrocute;
