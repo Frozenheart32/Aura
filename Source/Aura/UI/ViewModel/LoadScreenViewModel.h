7c653043f7c3aa2b384bc1a7122458e2f2ff0430
Frozenheart32 <alexroid32@gmail.com>
2024-09-15 18:23:23 +0300

    View Model Class

---
+++ Source/Aura/UI/ViewModel/LoadScreenViewModel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "LoadScreenViewModel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULoadScreenViewModel : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
