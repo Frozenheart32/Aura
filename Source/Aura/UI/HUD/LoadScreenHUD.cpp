db0e242d143cdac357d208ff35deeb36a3a65d25
Frozenheart32 <alexroid32@gmail.com>
2024-09-15 20:40:16 +0300

    Constructing a View Model

--- Source/Aura/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura/UI/HUD/LoadScreenHUD.cpp
@@ -3,3 +3,16 @@
 
 #include "UI/HUD/LoadScreenHUD.h"
 
+#include "Blueprint/UserWidget.h"
+#include "UI/ViewModel/LoadScreenViewModel.h"
+#include "UI/Widgets/LoadScreenWidget.h"
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<ULoadScreenViewModel>(this, LoadScreenViewModelClass);
+
+	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+}
--- Source/Aura/UI/HUD/LoadScreenHUD.h
+++ Source/Aura/UI/HUD/LoadScreenHUD.h
@@ -6,6 +6,10 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class ULoadScreenViewModel;
+class ULoadScreenWidget;
+class UUserWidget;
+
 /**
  * 
  */
@@ -13,5 +17,21 @@ UCLASS()
 class AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenViewModel> LoadScreenViewModel;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ULoadScreenViewModel> LoadScreenViewModelClass;
+
+	virtual void BeginPlay() override;
 };
